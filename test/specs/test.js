f46b0f49aa4a5e3243695413c001c90e9f4d3fa6
Jeff Duke <jeff.duke@gmail.com>
2016-08-16 14:29:20 -0600

    fixed build tools with Jhun

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,6 +2,8 @@ const assert =  require('assert')
 
 describe('welcome page', function(){
   it('should be able to grab the page title', function(){
-  	
+  	browser.url('/')
+    var title = browser.getTitle()
+    assert.equal(title, 'Cautious Parakeet Chat')
   });
 });

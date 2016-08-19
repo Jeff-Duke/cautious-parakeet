f2891d4d8be127b8edfe503c9a115d52f0990033
Jeff Duke <jeff.duke@gmail.com>
2016-08-18 18:54:50 -0600

    fix integration test

--- test/unit-tests/index.js
+++ test/unit-tests/index.js
@@ -1,25 +1,10 @@
-const assert = require('chai').assert
-const Chat = require('../../lib/scripts/chat')
+const assert = require('chai').assert;
+const Chat = require('../../lib/scripts/chat');
+const ChatRoom = require('../../lib/scripts/chat');
 
 describe('our test bundle', function () {
   it('should work', function () {
     assert(true);
   });
-  it('should gather input values and add new chat to chatArray', function () {
-    var username = 'login2';
-    var message = 'good morning';
-    var chat = new Chat(username, message);
 
-    Chat.ChatRoom.collectNewChat(chat);
-    assert.equal(ChatRoom.chatArray, [chat]);
-  });
 });
-
-// collectNewChat: function(chat) {
-//   this.chatArray.unshift(chat);
-// },
-//
-
-// collectNewChat: function() {
-//   this.chatArray.unshift(new Chat($usernameField.val(), $chatInput.val()));
-// },
